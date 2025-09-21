410ac0c3e37d67e4664bd368e59d72cba4df1d26
Karthik <123913145+karthikkemidi@users.noreply.github.com>
2025-09-21 16:09:08 +0000

    f

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
-package com.example;
 public class App {
-public static void main(String[] args) {
-System.out.println(&quot;Hello, Jenkins CI!&quot;);
-}
-public String getMessage() {
-return &quot;Hello, Jenkins CI!&quot;;
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI!");
+    }
+
+    public String getMessage() {
+        return "Hello, Jenkins CI!";
+    }
 }
--- AppTest.java
+++ AppTest.java
@@ -1,10 +1,11 @@
-package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class AppTest {
-@Test
-public void testMessage() {
-App app = new App();
-assertEquals(&quot;Hello, Jenkins CI!&quot;, app.getMessage());
-}
+
+    @Test
+    public void testGetMessage() {
+        App app = new App();
+        assertEquals("Hello, Jenkins CI!", app.getMessage());
+    }
 }
