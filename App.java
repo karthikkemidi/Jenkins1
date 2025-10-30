dd7d4ca73ec495ed2a90bcdd0ae1b38005f66356
karthikkemidi <kemidikarthik2004@gmail.com>
2025-10-30 11:41:48 +0530

    Initail commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI!");
+    }
+
+    public String getMessage() {
+        return "Hello, Jenkins CI!";
+    }
+}
---
+++ AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testGetMessage() {
+        App app = new App();
+        assertEquals("Hello, Jenkins CI!", app.getMessage());
+    }
+}
